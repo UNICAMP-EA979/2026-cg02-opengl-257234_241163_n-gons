cda914f828103738cb738c42c550186907455b3b
Everton Colombo <e.rcolombo2@gmail.com>
2026-05-21 17:27:31 -0300

    feat: implement texture __init__

--- src/urenderer/renderer/opengl/texture.py
+++ src/urenderer/renderer/opengl/texture.py
@@ -40,17 +40,23 @@ class Texture:
 
         ## SEU CÓDIGO AQUI ######################################################
         # Cria a textura
+        texture_id = GL.glGenTextures(1)
 
         # Realiza o bind no contexto
+        GL.glBindTexture(GL.GL_TEXTURE_2D, texture_id)
 
         # Define os parâmetros da textura
         self.parameters: dict[IntConstant, int] = {}
         for parameter, value in Texture._default_parameters.items():
-            GL.glTexParameteri(...)
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, parameter, value)
 
         # Especifica os dados da textura
-
+        GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, 
+                        self._internal_format, texture_data.shape[1], 
+                        texture_data.shape[0], 0, self._data_format, 
+                        GL.GL_UNSIGNED_BYTE, texture_data)
         # Gera os mipmaps da textura
+        GL.glGenerateMipmap(GL.GL_TEXTURE_2D)
 
         #########################################################################
 
